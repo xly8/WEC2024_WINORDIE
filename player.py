81debc816eb727dec1ad8cfd42ae62c5de68424d
LEYANG XING <lxing24@uwo.ca>
2024-11-08 20:01:00 -0500

    fix: Merge conflicts

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 from projectile import Projectile
 import time
 
@@ -7,6 +7,12 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("Resources/AstronautImages/down1.png")
         self.rect = self.image.get_rect(center=(2080, 1168))
+        self.direction = pygame.math.Vector2()
+        self.speed = 4
+        self.hitbox = self.rect.inflate(-35, -20)
+        self.current_room = 0
+        self.obstacle_sprites = None
+        self.door_sprites = None
         self.health = 3
         self.max_health = 3
         self.ammo = 50
@@ -15,48 +21,56 @@ class Player(pygame.sprite.Sprite):
         self.shooting_speed = 1
         self.projectile_count = 1
 
-        # Character Animation settings
+        # Animation settings
         self.last_pressed_key = None
         self.pressed_keys = set()
         self.animation_index = 0
         self.animation_speed = 0.08
         self.last_update = time.time()
         self.facing = 'down'  # default facing direction
-        self.walk_left = [
-            pygame.image.load("Resources/AstronautImages/left1.png"),
-            pygame.image.load("Resources/AstronautImages/left2.png"),
-            pygame.image.load("Resources/AstronautImages/left3.png"),
-            pygame.image.load("Resources/AstronautImages/left4.png")
+        self.walk_left = self.load_walk_images('left')
+        self.walk_right = self.load_walk_images('right')
+        self.walk_up = self.load_walk_images('up')
+        self.walk_down = self.load_walk_images('down')
+
+    def load_walk_images(self, direction):
+        return [
+            pygame.image.load(f"Resources/AstronautImages/{direction}{i}.png")
+            for i in range(1, 5)
         ]
-        self.walk_right = [
-            pygame.image.load("Resources/AstronautImages/right1.png"),
-            pygame.image.load("Resources/AstronautImages/right2.png"),
-            pygame.image.load("Resources/AstronautImages/right3.png"),
-            pygame.image.load("Resources/AstronautImages/right4.png")
-        ]
-        self.walk_up = [
-            pygame.image.load("Resources/AstronautImages/up1.png"),
-            pygame.image.load("Resources/AstronautImages/up2.png"),
-            pygame.image.load("Resources/AstronautImages/up3.png"),
-            pygame.image.load("Resources/AstronautImages/up4.png")
-        ]
-        self.walk_down = [
-            pygame.image.load("Resources/AstronautImages/down1.png"),
-            pygame.image.load("Resources/AstronautImages/down2.png"),
-            pygame.image.load("Resources/AstronautImages/down3.png"),
-            pygame.image.load("Resources/AstronautImages/down4.png")
-        ]
-    
+
     def set_sprites(self, obstacle_sprites, door_sprites):
         self.obstacle_sprites = obstacle_sprites
         self.door_sprites = door_sprites
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+            self.facing = 'up'
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+            self.facing = 'down'
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+            self.facing = 'left'
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+            self.facing = 'right'
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def animate(self):
         current_time = time.time()
         if current_time - self.last_update > self.animation_speed:
             self.animation_index = (self.animation_index + 1) % 4
             self.last_update = current_time
-            
             if self.facing == 'left':
                 self.image = self.walk_left[self.animation_index]
             elif self.facing == 'right':
@@ -66,9 +80,12 @@ class Player(pygame.sprite.Sprite):
             elif self.facing == 'down':
                 self.image = self.walk_down[self.animation_index]
 
-    def move(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
+    def move(self):
+        self.hitbox.x += self.direction.x * self.speed
+        self.collision('h')
+        self.hitbox.y += self.direction.y * self.speed
+        self.collision('v')
+        self.rect.center = self.hitbox.center
 
     def shoot(self):
         if self.ammo > 0:
@@ -76,65 +93,6 @@ class Player(pygame.sprite.Sprite):
                 self.projectiles.append(Projectile(self.rect.center))
             self.ammo -= 1
 
-    def update(self):
-        keys = pygame.key.get_pressed()
-        moving = False
-        
-        # Track key presses and releases
-        for key in [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]:
-            if keys[key]:
-                if key not in self.pressed_keys:
-                    self.last_pressed_key = key
-                    self.pressed_keys.add(key)
-            else:
-                self.pressed_keys.discard(key)
-                if key == self.last_pressed_key:
-                    self.last_pressed_key = None if not self.pressed_keys else list(self.pressed_keys)[-1]
-
-        # Move based on last pressed key
-        if self.last_pressed_key == pygame.K_a:
-            self.move(-5, 0)
-            self.facing = 'left'
-            moving = True
-        elif self.last_pressed_key == pygame.K_d:
-            self.move(5, 0)
-            self.facing = 'right'
-            moving = True
-        elif self.last_pressed_key == pygame.K_w:
-            self.move(0, -5)
-            self.facing = 'up'
-            moving = True
-        elif self.last_pressed_key == pygame.K_s:
-            self.move(0, 5)
-            self.facing = 'down'
-            moving = True
-
-        # Shooting
-        if keys[pygame.K_SPACE]:
-            self.shoot()
-
-        # Animation handling
-        if moving:
-            self.animate()
-        else:
-            # Reset to standing frame
-            if self.facing == 'left':
-                self.image = self.walk_left[0]
-            elif self.facing == 'right':
-                self.image = self.walk_right[0]
-            elif self.facing == 'up':
-                self.image = self.walk_up[0]
-            elif self.facing == 'down':
-                self.image = self.walk_down[0]
-
-        for projectile in self.projectiles:
-            projectile.update()
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-        for projectile in self.projectiles:
-            projectile.draw(screen)
-            
     def apply_power_up(self, power_type):
         if power_type == "shooting_speed":
             self.shooting_speed = max(0.1, self.shooting_speed * 0.9)
@@ -148,3 +106,36 @@ class Player(pygame.sprite.Sprite):
             self.max_health += 1
         elif power_type == "max_ammo":
             self.max_ammo += 10
+
+    def collision(self, direction):
+        if direction == 'h':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:
+                        self.hitbox.right = sprite.hitbox.left
+                    elif self.direction.x < 0:
+                        self.hitbox.left = sprite.hitbox.right
+        if direction == 'v':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0:
+                        self.hitbox.bottom = sprite.hitbox.top
+                    elif self.direction.y < 0:
+                        self.hitbox.top = sprite.hitbox.bottom
+        for sprite in self.door_sprites:
+            if sprite.hitbox.colliderect(self.hitbox):
+                if sprite.door_name == 'Merchant':
+                    self.current_room = 0
+
+    def update(self):
+        self.input()
+        self.move()
+        if self.direction.magnitude() > 0:
+            self.animate()
+        for projectile in self.projectiles:
+            projectile.update()
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        for projectile in self.projectiles:
+            projectile.draw(screen)
